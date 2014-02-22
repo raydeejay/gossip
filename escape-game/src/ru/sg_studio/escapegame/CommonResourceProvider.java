c0c079acd07593070d832b084096e40466f73e5e
Semion Nadezhdin <me@sg-studio.ru>
2014-02-22 19:47:14 +0400

    Hack: ability to run image-only mode

--- escape-game/src/ru/sg_studio/escapegame/CommonResourceProvider.java
+++ escape-game/src/ru/sg_studio/escapegame/CommonResourceProvider.java
@@ -5,7 +5,7 @@ public class CommonResourceProvider {
 	private GossipVM VM;
 
 	public void load(GameScreen bindedScreen) {
-		VM = new GossipVM();
+		VM = new GossipVM(false);//Change to true to load image only!
 		VM.load();
 		VM.provideInitialScreen(bindedScreen);
 	}
