465f91ac1d75142f5a3c4b50def1feeaba5d0bc7
Semion Nadezhdin <me@sg-studio.ru>
2014-02-24 14:04:31 +0400

    CUH and Schedule linking capabilities

--- escape-game/src/ru/sg_studio/escapegame/eventSystem/CycleUpdateHandler.java
+++ escape-game/src/ru/sg_studio/escapegame/eventSystem/CycleUpdateHandler.java
@@ -1,13 +1,30 @@
 package ru.sg_studio.escapegame.eventSystem;
 
+import javax.script.ScriptException;
+
 import ru.sg_studio.escapegame.GossipVM;
 
 public class CycleUpdateHandler {
 
+	//By default CUH is linked to Schedule, so there is no subset yet available
+	
 	public void run(GossipVM myHost) {
 		// TODO hook to VM execute()
 		//Thread marshalling is not requared here
-		System.out.println("DUH!");
+		try {
+			myHost.directEvaluation(cachedJob);
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	private int callID;
+	private String cachedJob;
+	public void setCallID(int callID){
+		this.callID=callID;
+		cachedJob = "Schedule call: "+this.callID;
+	}
+	
 
 }
