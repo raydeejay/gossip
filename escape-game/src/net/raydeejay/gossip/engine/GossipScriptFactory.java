afcfac63553ec42471106c2ae5a1e789ded206ad
Semion Nadezhdin <me@sg-studio.ru>
2014-03-13 14:28:35 +0400

    GossipScriptFactory cleaned unused imports

--- escape-game/src/net/raydeejay/gossip/engine/GossipScriptFactory.java
+++ escape-game/src/net/raydeejay/gossip/engine/GossipScriptFactory.java
@@ -7,9 +7,7 @@
 */
 package net.raydeejay.gossip.engine;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
